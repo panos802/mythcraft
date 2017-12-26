aec5392693a130d61743745c9476d49a79f90710
panos802 <panos802@gmail.com>
2017-12-26 18:47:06 +0200

    firebase works, trying to create new collection (commented out for now)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,11 +20,27 @@ export class AppComponent implements OnInit{
   postsCol: AngularFirestoreCollection<Post>;
   posts: Observable<Post[]>;
 
+  // messagesCol: AngularFirestoreCollection<string>;
+  // messages: Observable<string[]>;
+
+  title: string;
+  content: string;
+
   constructor(private afs: AngularFirestore) {}
 
   ngOnInit() {
     this.postsCol = this.afs.collection('posts');
     this.posts = this.postsCol.valueChanges();
+
+    // this.messagesCol = this.afs.collection('messages');
+    // this.messages = this.messagesCol.valueChanges();
+  }
+
+  addPost() {
+    this.afs.collection('posts').add({'title': this.title, 'content': this.content});
+    this.title = '';
+    this.content = '';
+    // this.afs.collection('messages').add({'message': 'addition'});
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule, AngularFirestore } from 'angularfire2/firestore';
@@ -22,7 +23,8 @@ const firebaseConfig = {
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(firebaseConfig),
-    AngularFirestoreModule
+    AngularFirestoreModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
